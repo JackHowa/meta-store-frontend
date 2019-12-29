1a68b98860aae83d17a67ea3e07397c37e310293
Jack Howard <jackhowaa@gmail.com>
2019-12-29 11:08:12 -0600

    parcel attempt for building custom

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>yo</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
