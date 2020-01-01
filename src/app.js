2540df448e83242f358598897051cc7d5c1c6890
Jack Howard <jackhowaa@gmail.com>
2019-12-31 19:19:59 -0600

    Add prettier and eslint for dev process

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-	return (
-		<p>Working!</p>
-	)
+  return <p>Working!</p>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
