3324ad9567c8bcb31ac3aa3eff625b76eaeb4c9f
Jack Howard <jackhowaa@gmail.com>
2020-01-08 20:24:35 -0600

    Use basic styles in components

--- src/components/ListManager.js
+++ src/components/ListManager.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SiteListContainer, SiteCreationContainer } from '../containers';
+import { AppStyles } from '../styles';
 
 export default function ListManager() {
   return (
-    <>
+    <AppStyles>
       <SiteListContainer />
       <SiteCreationContainer />
-    </>
+    </AppStyles>
   );
 }
