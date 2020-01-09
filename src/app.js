6f50ff8d83846d8a5e49405ce8415f1f80515d50
Jack Howard <jackhowaa@gmail.com>
2020-01-08 20:24:05 -0600

    Remove unnec css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ListManager from './components/ListManager';
