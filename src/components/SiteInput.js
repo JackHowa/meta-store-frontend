0907a7387e2b4a060b1151876adbe46cb0bb8c59
Jack Howard <jackhowaa@gmail.com>
2020-01-12 18:14:16 -0600

    Include additional text input linaria styles

--- src/components/SiteInput.js
+++ src/components/SiteInput.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ButtonStyles, BigInputStyles, TextInputStyles } from '../styles';
 
 export default function SiteInput({
   createSite,
@@ -9,43 +10,52 @@ export default function SiteInput({
   const [selectedCategoryIds, setSelectedCategoryIds] = useState([]);
 
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        createSite(newSiteName, selectedCategoryIds);
-        setNewSiteName('');
-        setSelectedCategoryIds([]);
-      }}
-    >
-      <input
-        type="text"
-        value={newSiteName}
-        onChange={e => setNewSiteName(e.target.value)}
-      />
-      {categoryIDs.map(id => {
-        const targetCategory = categoryEntities[id];
+    <BigInputStyles>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          createSite(newSiteName, selectedCategoryIds);
+          setNewSiteName('');
+          setSelectedCategoryIds([]);
+        }}
+      >
+        <div>
+          <input
+            className={TextInputStyles}
+            type="text"
+            value={newSiteName}
+            onChange={e => setNewSiteName(e.target.value)}
+          />
+        </div>
+        <div>
+          {categoryIDs.map(id => {
+            const targetCategory = categoryEntities[id];
 
-        return (
-          <label key={id} htmlFor={id}>
-            {targetCategory.displayName}
+            return (
+              <div key={id}>
+                <label htmlFor={id}>
+                  {targetCategory.displayName}
 
-            <input
-              type="checkbox"
-              id={id}
-              checked={selectedCategoryIds.includes(id)}
-              // todo: refactor to ensure this isn't reruning on render
-              onChange={e => {
-                if (e.target.checked) {
-                  setSelectedCategoryIds(selectedCategoryIds.concat(id));
-                } else {
-                  // uncheck category
-                }
-              }}
-            />
-          </label>
-        );
-      })}
-      <button type="submit">Add</button>
-    </form>
+                  <input
+                    type="checkbox"
+                    id={id}
+                    checked={selectedCategoryIds.includes(id)}
+                    // todo: refactor to ensure this isn't reruning on render
+                    onChange={e => {
+                      if (e.target.checked) {
+                        setSelectedCategoryIds(selectedCategoryIds.concat(id));
+                      } else {
+                        // uncheck category
+                      }
+                    }}
+                  />
+                </label>
+              </div>
+            );
+          })}
+        </div>
+        <ButtonStyles type="submit">Add</ButtonStyles>
+      </form>
+    </BigInputStyles>
   );
 }
